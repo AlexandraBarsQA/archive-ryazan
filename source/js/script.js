f71a2304a2f3db95818b06c031923ce924e7ecb0
Barsik1976 <shlippenbah@mail.ru>
2022-08-05 18:11:08 +0300

    Верстка форм

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper(".slider", {
   navigation: {
-    nextEl: ".toggle-gallery__item--next.swiper-button-next",
     prevEl: ".toggle-gallery__item--prev.swiper-button-prev",
+    nextEl: ".toggle-gallery__item--next.swiper-button-next",
   },
   pagination: {
     el: ".swiper-pagination",
@@ -9,25 +9,85 @@ var swiper = new Swiper(".slider", {
     clickable: true,
   },
   loop: true,
-  speed: 600,
   //autoHeight: true,
   watchSlidesProgress: true,
   slidesPerView: 3,
+  speed: 1500,
   spaceBetween: 20,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
 
   // Responsive breakpoints
+
+  breakpoints: {
+    // when window width is >= 320px
+    767: {
+      loop: true,
+      slidesPerView: 3,
+      //spaceBetween: 20
+    },
+
+    992: {
+      loop: true,
+      slidesPerView: 2,
+      //spaceBetween: 20
+    },
+
+    // when window width is >= 640px
+    1080: {
+      loop: true,
+      slidesPerView: 3,
+      speed: 1500,
+      //spaceBetween: 40
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+    },
+  },
 });
 
 // Слайдер Фотоархива на странице About-archive
 
 var galleryThumbs = new Swiper(".photo-gallery", {
-  slidesPerView: 5,
-  spaceBetween: 64,
+  slidesPerView: 1,
+  spaceBetween: 20,
   speed: 600,
   loop: true,
   loopedSlides: 1, //looped slides should be the same
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+
+  breakpoints: {
+    // when window width is >= 320px
+
+    480: {
+      loop: true,
+      slidesPerView: 2,
+      //spaceBetween: 20,
+    },
+
+    580: {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+
+    767: {
+      loop: true,
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+
+    // when window width is >= 640px
+    1180: {
+      loop: true,
+      slidesPerView: 5,
+      spaceBetween: 64,
+    },
+  },
 });
 
 var gallery = new Swiper(".gallery-desc", {
@@ -46,8 +106,10 @@ var gallery = new Swiper(".gallery-desc", {
 
 // Слайдер Swiper + popup
 
+// Вариант 1
+
 let moreBtns = document.querySelectorAll(".photo-gallery__item");
-let modals = document.querySelectorAll(".popup__list");
+let modals = document.querySelectorAll(".popup");
 let closeBtns = document.querySelectorAll(".popup__btn-close");
 //добавим скрывающий класс
 modals.forEach((modal) => modal.classList.add("hide"));
@@ -82,6 +144,8 @@ window.addEventListener("keydown", function (e) {
   }
 });
 
+// Вариант 2
+
 /*
 var link = document.querySelector(".photo-gallery__item--1");
 var popup = document.querySelector(".popup__item--1");
@@ -111,3 +175,29 @@ window.addEventListener("keydown", function (evt) {
   }
 });
  */
+
+// Вариант 3
+
+/*всё тоже самое, кроме...*/
+let allElems = document.querySelectorAll(".type_3 .headingElem");
+
+allElems.forEach((elem) => {
+  elem.addEventListener("click", function () {
+    /*находим все активные элементы*/
+    let descActive = document.querySelectorAll(".type_3 .descElem.active");
+    /*прогоняем через цикл и удаляем класс active*/
+    descActive.forEach((elem) => {
+      elem.classList.remove("active");
+    });
+
+    let parentElem = this.parentNode;
+
+    let contentBlock = parentElem.querySelector(".descElem");
+
+    if (contentBlock.classList.contains("active")) {
+      contentBlock.classList.remove("active");
+    } else {
+      contentBlock.classList.add("active");
+    }
+  });
+});
